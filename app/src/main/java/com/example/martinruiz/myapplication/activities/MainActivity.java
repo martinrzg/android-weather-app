0a2dd43543cd5cb05a02ef622642e88b43fabc4c
Martín <martin.ruiz8811@gmail.com>
2017-08-19 19:14:32 -0500

    Added layouts mainActivity a recycler view, and layout for the item (WeatherCard)

--- app/src/main/java/com/example/martinruiz/myapplication/activities/MainActivity.java
+++ app/src/main/java/com/example/martinruiz/myapplication/activities/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.martinruiz.myapplication;
+package com.example.martinruiz.myapplication.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Window;
+
+import com.example.martinruiz.myapplication.R;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +13,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getSupportActionBar().hide();
+
+
 
     }
 
