8f83c2fb1d6b1b4d9440b90c7300335865e47fa2
Martín <martin.ruiz8811@gmail.com>
2017-08-20 12:28:45 -0500

    Added onSwipeListener Interface for implement onItemDelete if the user swipes a card

---
+++ app/src/main/java/com/example/martinruiz/myapplication/interfaces/onSwipeListener.java
@@ -0,0 +1,9 @@
+package com.example.martinruiz.myapplication.interfaces;
+
+/**
+ * Created by MartinRuiz on 8/20/2017.
+ */
+
+public interface onSwipeListener {
+    void onItemDelete(int position);
+}
