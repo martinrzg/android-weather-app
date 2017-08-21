64c7a06f4b6ea4818ab05d4cc9f390663371e8e1
Martín <martin.ruiz8811@gmail.com>
2017-08-20 19:34:15 -0500

    Added WeatherDetails activity

---
+++ app/src/main/java/com/example/martinruiz/myapplication/activities/WeatherDetails.java
@@ -0,0 +1,16 @@
+package com.example.martinruiz.myapplication.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.martinruiz.myapplication.R;
+
+public class WeatherDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weather_details);
+
+    }
+}
