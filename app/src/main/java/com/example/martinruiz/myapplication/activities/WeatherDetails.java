18a25635082af1f87490f1642d29afe40d354057
Martín <martin.ruiz8811@gmail.com>
2017-08-21 02:16:17 -0500

    Unbundle the extras and getSeriazable obj

--- app/src/main/java/com/example/martinruiz/myapplication/activities/WeatherDetails.java
+++ app/src/main/java/com/example/martinruiz/myapplication/activities/WeatherDetails.java
@@ -4,13 +4,21 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.martinruiz.myapplication.R;
+import com.example.martinruiz.myapplication.models.CityWeather;
 
 public class WeatherDetails extends AppCompatActivity {
-
+    private CityWeather cityWeather;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather_details);
+        Bundle bundle = getIntent().getExtras();
+        if(! bundle.isEmpty()){
+            cityWeather = (CityWeather) bundle.getSerializable("city");
+        }
+
+        System.out.println(cityWeather.getCity().getName());
+        System.out.println(cityWeather.getWeeklyWeather().get(0).getTemp().getDay());
 
     }
 }
